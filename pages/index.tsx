55e79e2a23809f13266d7bd74577ec6737169b21
bo-yakitarako <bo-yakitarako@users.noreply.github.com>
2021-10-01 01:32:55 +0900

    中央配置のやり方変えた

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,9 +14,9 @@ const Home: NextPage = () => {
       </Head>
       <div className="h-screen">
         <FadeIn>
-          <header className="flex absolute flex-col justify-center items-center w-full h-full">
+          <header className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
             <h1 className="text-4xl sm:text-6xl">bo-yakitarako</h1>
-            <p className="mt-3 sm:mt-4 text-base sm:text-lg">
+            <p className="mt-2 sm:mt-3 text-base sm:text-lg text-center">
               Programming & Golf
             </p>
           </header>
