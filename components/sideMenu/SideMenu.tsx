d27511f5a1cc39c91edbda11666492d5dddcabf1
bo-yakitarako <bo-yakitarako@users.noreply.github.com>
2021-10-02 03:50:43 +0900

    Revert "まあまたなんか変えたかったら変えましょ"
    This reverts commit ff94c0a5048644a0516c13eac53450766bb6debd.

--- components/sideMenu/SideMenu.tsx
+++ components/sideMenu/SideMenu.tsx
@@ -1,19 +1,13 @@
-import { Page, SideMenuItem } from './SideMenuItem';
-
-const products: Page[] = [
-  { title: '迷路ゲーム', path: '/products/maze' },
-  { title: '迷路生成', path: '/products/maze_maker' },
-  { title: 'Skull King 計算機', path: '/products/skullking' },
-];
+import { SideMenuItem } from './SideMenuItem';
 
 const SideMenu: React.FC = () => {
   return (
-    <nav className="hidden sm:block fixed top-3 left-3 z-20 w-60 bg-transparent">
+    <nav className="hidden sm:block fixed top-3 left-3 w-60 bg-transparent">
       <ul className="flex overflow-y-scroll relative flex-col w-full max-h-screen list-none">
-        <SideMenuItem href="/about">About</SideMenuItem>
-        <SideMenuItem href="/golf">Golf</SideMenuItem>
+        <SideMenuItem>About</SideMenuItem>
+        <SideMenuItem>Golf</SideMenuItem>
         <SideMenuItem>Sparebeat</SideMenuItem>
-        <SideMenuItem contents={products}>Products</SideMenuItem>
+        <SideMenuItem>Product</SideMenuItem>
       </ul>
     </nav>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -10,9 +10,11 @@ const Home: NextPage = () => {
         <meta name="description" content="bo-yakitarakoのホームページ" />
       </Head>
       <FadeIn>
-        <header className="flex flex-col justify-center items-center h-screen">
-          <h1 className="text-4xl sm:text-6xl">bo-yakitarako</h1>
-          <p className="mt-2 sm:mt-3 text-base sm:text-lg text-center">
+        <header className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+          <h1 className="text-4xl sm:text-6xl whitespace-nowrap">
+            bo-yakitarako
+          </h1>
+          <p className="mt-2 sm:mt-3 text-base sm:text-lg text-center whitespace-nowrap">
             Programming & Golf
           </p>
         </header>
