ed4908400aea77a55f9858a4acf29764d0d588c1
bo-yakitarako <bo-yakitarako@users.noreply.github.com>
2021-10-02 00:22:17 +0900

    404ページ作った

---
+++ components/Link.tsx
@@ -0,0 +1,40 @@
+import { faExternalLinkAlt } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import LinkRaw from 'next/link';
+
+type Props = {
+  href: string;
+  className?: string;
+  external?: boolean;
+};
+
+const style = (className?: string) =>
+  `${
+    className ?? 'text-2xl'
+  } inline-block relative after:absolute after:-bottom-0.5 after:left-0 mb-1 after:w-full after:h-px no-underline after:bg-gray-50 after:transition-transform after:duration-300 after:origin-top-left hover:after:scale-100 after:scale-y-100 after:scale-x-0 hover:cursor-pointer`;
+
+const Link: React.FC<Props> = ({ href, className, external, children }) => {
+  if (external) {
+    return (
+      <a
+        target="_blank"
+        rel="noopener noreferrer"
+        href={href}
+        className={style(className)}
+      >
+        {children}{' '}
+        <FontAwesomeIcon
+          className="font-normal fill-current"
+          icon={faExternalLinkAlt}
+        />
+      </a>
+    );
+  }
+  return (
+    <LinkRaw href={href} passHref>
+      <span className={style(className)}>{children}</span>
+    </LinkRaw>
+  );
+};
+
+export { Link };
---
+++ pages/404.tsx
@@ -0,0 +1,20 @@
+import { NextPage } from 'next';
+import { Link } from '../components/Link';
+
+const NotFound: NextPage = () => {
+  return (
+    <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+      <p className="text-2xl sm:text-3xl font-normal whitespace-nowrap">
+        404 | Not Found
+      </p>
+      <div className="m-2 text-center">
+        <Link className="text-base" href="/">
+          Back to Home
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+// eslint-disable-next-line import/no-default-export
+export default NotFound;
