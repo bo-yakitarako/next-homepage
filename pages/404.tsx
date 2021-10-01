32183bb291b6bd3edc15acd8180fd62591ec996c
bo-yakitarako <bo-yakitarako@users.noreply.github.com>
2021-10-02 02:48:25 +0900

    404ページにもタイトルとかつけとくよ

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,18 +1,25 @@
 import { NextPage } from 'next';
+import Head from 'next/head';
 import { Link } from '../components/Link';
 
 const NotFound: NextPage = () => {
   return (
-    <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-      <p className="text-2xl sm:text-3xl font-normal whitespace-nowrap">
-        404 | Not Found
-      </p>
-      <div className="m-2 text-center">
-        <Link className="text-base" href="/">
-          Back to Home
-        </Link>
+    <>
+      <Head>
+        <title>bo-yakitarako 404</title>
+        <meta name="description" content="そんなページは存在しないんだよ？" />
+      </Head>
+      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+        <p className="text-2xl sm:text-3xl font-normal whitespace-nowrap">
+          404 | Not Found
+        </p>
+        <div className="m-2 text-center">
+          <Link className="text-base" href="/">
+            Back to Home
+          </Link>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
